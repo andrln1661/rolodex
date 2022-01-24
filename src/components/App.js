a086d393b2582dc0f54024fc29be724577ee4f2e
andrln1661 <andrln1661@gmail.com>
2022-01-24 20:11:07 +0300

    Added GHPAGES

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,54 @@
-import React from 'react';
-import {CardList} from './CardList';
-import {SearchBox} from './SearchBox';
-import {Scroll} from './Scroll'
-import '../styles/App.css';
-
+import React from "react";
+import { CardList } from "./CardList";
+import { SearchBox } from "./SearchBox";
+import { Scroll } from "./Scroll";
+import "../styles/App.css";
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({robots: users}))
-        
-    }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-    render() {
-        const filterredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
-        if (this.state.robots.length === 0) { 
-            return <div className="Loading-box"><h1 className="Loading">Loading</h1></div>
-        } else {
-        return (
-                <div className="main">
-                    <div className="header">
-                        <h1 id="title">RoboFriends</h1>
-                        <SearchBox searchChange={this.onSearchChange}/>
-                    </div>
-                    <Scroll>
-                        <CardList robots={filterredRobots}/>
-                    </Scroll>
-                </div>
-                
-            );
-        }
+  render() {
+    const filterredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    if (this.state.robots.length === 0) {
+      return (
+        <div className="Loading-box">
+          <h1 className="Loading">Loading</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div className="main">
+          <div className="header">
+            <h1 id="title">Rolodex</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+          </div>
+          <Scroll>
+            <CardList robots={filterredRobots} />
+          </Scroll>
+        </div>
+      );
     }
+  }
 }
 
 export default App;
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,9 +1,6 @@
-import React from 'react';
+import React from "react";
+import "../styles/Scroll.css";
 
 export const Scroll = (props) => {
-    return (
-        <div style={{overflowY: 'scroll', height: '800px'}}>
-            {props.children}
-        </div>
-    );
+  return <div className={"scroll"}>{props.children}</div>;
 };
